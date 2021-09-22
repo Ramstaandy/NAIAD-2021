e01e1a1eb33cafc1663f90e11f25732692d6dd0c
Andreas Mäkilä <ramsta.andy@hotmail.com>
2021-09-22 08:28:18 +0200

    split http serer into its own file, second try

--- Python/main.py
+++ Python/main.py
@@ -10,36 +10,9 @@ comments describing function and purpose of everything added.
     --A. Makila
 '''
 
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import json
 '''import motorsubproc
 import trilatsubproc'''
-
-# Default HTTPServer, modded to store the latest received route
-# so it can be read as a loop-exit condition and stored for use
-class HTTPPOSTServer(HTTPServer):
-    def __init__(self, *args, **kwargs):
-        super(HTTPPOSTServer, self).__init__(*args, **kwargs)
-        self.recv_route = {}
-
-# Default SimpleHTTPRequestHandler, modded with POST responses
-# so it can receive routes from the ground crew
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Send the route with POST!')
-
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        # TODO: check that the received data json and is structured right
-        self.server.recv_route = json.loads(body.decode("utf-8"))
-        
-        # TODO: send non-200 response if data is malformed
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Data received!')
+import http_recv_server as recvserv
 
 '''motorsubproc.Start()
 trilatsubproc.Start()
@@ -48,7 +21,7 @@ trilatsubproc.Start()
 motorsubproc.SetDepth(0.0)'''
 
 #Receives the route from the ground crew
-httpd = HTTPPOSTServer(("localhost", 4000), SimpleHTTPRequestHandler)
+httpd = revsrev.HTTPPOSTServer(("localhost", 4000), SimpleHTTPRequestHandler)
 route = {}
 while route == {}:
     httpd.handle_request()
